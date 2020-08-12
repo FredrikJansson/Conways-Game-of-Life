38ff09e33cc77d3e238af711d1437db53f9cd065
FredrikJansson <fredrik@janssoniaby.se>
2020-08-12 18:04:27 +0200

    Added running with arguments.

--- main.py
+++ main.py
@@ -5,10 +5,31 @@ Fredrik Jansson
 
 from board import Board
 
+import sys
 
-def main():
+
+def get_input_values():
     in_row = int(input("Enter how many rows: "))
     in_col = int(input("Enter how many columns: "))
+    return in_row, in_col
+
+
+def main():
+    in_row = 0
+    in_col = 0
+
+    if len(sys.argv) > 1:
+        in_row = sys.argv[1]
+        in_col = sys.argv[2]
+    else:
+        in_row, in_col = get_input_values()
+
+    try:
+        in_row = int(in_row)
+        in_col = int(in_col)
+    except Exception:
+        print('Invalid arguments.')
+        in_row, in_col = get_input_values()
 
     gol_board = Board(in_row, in_col)
 
